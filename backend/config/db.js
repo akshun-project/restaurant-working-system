a5413a7c7c853580f1deeadf7f95cef3e317e8d0
Akshun Jindal <akshunjindal7889@gmail.com>
2026-05-31 23:41:37 +0530

    Fix TiDB SSL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,21 +3,22 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
 
   ssl: {
+    minVersion: "TLSv1.2",
     rejectUnauthorized: false,
   },
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.error("Database connection failed:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("✅ MySQL Connected");
   }
 });
 
